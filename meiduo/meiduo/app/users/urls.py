60efbe9fef3159aa807b0e6bae9673d49983be31
caimengxuan <875790211@qq.com>
2020-12-14 22:49:08 +0800

    12月14 3.4

--- meiduo/meiduo/app/users/urls.py
+++ meiduo/meiduo/app/users/urls.py
@@ -3,5 +3,6 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('register/',views.RegisterView.as_view(),name='register')
+    path('register/',views.RegisterView.as_view(),name='register'),
+
 ]
