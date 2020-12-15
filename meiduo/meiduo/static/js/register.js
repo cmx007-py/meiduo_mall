e8273dff594e7bfb8752edf4c1ba6040afd652ad
caimengxuan <875790211@qq.com>
2020-12-15 11:29:32 +0800

    12月14 3.4

---
+++ meiduo/meiduo/static/js/register.js
@@ -0,0 +1,40 @@
+let vm=new Vue({
+    el:'#app',
+    data:{
+        username:'',
+        password1:'',
+        password2:'',
+        mobile:'',
+        allow:'',
+        error_name:false,
+        error_password1:false,
+        error_password2:false,
+        error_mobile:false,
+        error_allow:false,
+
+        error_name_message:'',
+        erroe_mobile_message:'',
+    }
+    methods:{
+        check_username(){
+
+        },
+        check_password1(){
+
+        }
+        check__password2(){
+
+        },
+        check_mobile(){
+
+        },
+        check_allow(){
+
+        },
+        on_submit(){
+
+        },
+
+
+    }
+});
